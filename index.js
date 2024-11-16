ce88dad725e96706b4f4e163b4fa652c6092fad8
Sizzler_55 <babarsmoke24@gmail.com>
2024-11-16 11:48:16 +0530

    Discord Bot Created

---
+++ index.js
@@ -0,0 +1,20 @@
+
+    import { Client, GatewayIntentBits } from "discord.js";
+    import dotenv from "dotenv";
+    dotenv.config();
+
+    const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+    });
+
+    client.on("messageCreate", (message) => {
+    if (message.author.bot) return;
+    message.reply(`Welcome ${message.author} `);
+    });
+
+    client.login(process.env.Client);
+
